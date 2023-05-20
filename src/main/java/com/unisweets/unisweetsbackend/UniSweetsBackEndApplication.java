997b051a7c6a5f064de78be09aa09478af8d62f4
TeaChan <valkova.tanya17@gmail.com>
2023-05-20 11:36:38 +0300

    Initial commit

---
+++ src/main/java/com/unisweets/unisweetsbackend/UniSweetsBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.unisweets.unisweetsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniSweetsBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniSweetsBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unisweets/unisweetsbackend/UniSweetsBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unisweets.unisweetsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniSweetsBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
